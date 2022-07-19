2698965b689f57929be0ed5da62e91d7b168fd1c
Dovi Cowan <dovi@fullynetworking.co.uk>
2022-07-20 00:46:09 +0100

    inital commit

---
+++ background-script.js
@@ -0,0 +1,50 @@
+/* chrome.browserAction.onClicked.addListener(function () {
+    console.log("Test");
+
+    // let title = browser.i18n.getMessage("notificationTitle");
+    // let content = browser.i18n.getMessage("notificationContent", message.url);
+}) */
+
+chrome.action.onClicked.addListener(async tab => {
+    try {
+        await chrome.scripting.insertCSS({
+            target: {
+                tabId: tab.id,
+            },
+            css: `.s-item a.s-item__link { border-style: solid; }`
+        });
+    } catch (error) {
+        console.error('CSS insert failed! ' + error);
+    }
+
+    try {
+        await chrome.scripting.executeScript({
+            target: {
+                tabId: tab.id,
+            },
+            func: () => {
+                document.querySelectorAll('.s-item a.s-item__link').forEach(element => {
+                    let href = element.getAttribute('href');
+                    href = href.split('/');
+                    let itemID = href[href.length-1].split('?')[0];
+                    console.log(itemID);
+                });
+            }
+        });
+    } catch (error) {
+        console.error('Get HREFs failed! ' + error);
+    }
+});
+
+chrome.webNavigation.onCompleted.addListener(function() {
+    chrome.notifications.create({
+        "type": "basic",
+        "iconUrl": "icons/ebay-48.png",
+        "title": 'My Notification',
+        "message": 'Hi, this is eBay!'
+    });
+}, {
+    url: [{
+        urlMatches : 'https://www.ebay.co.uk/'
+    }]
+});
